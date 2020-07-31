c2701a708fced4aa5405887470a7a3baaefd591f
TheNycteris <pau.domene.91@gmail.com>
2020-07-31 20:56:07 +0200

    Primer Cambio

---
+++ ProjecteDeProva/src/projectedeprova/ProjecteDeProva.java
@@ -0,0 +1,15 @@
+
+
+package projectedeprova;
+
+public class ProjecteDeProva {
+
+    public static void main(String[] args) {
+        
+        //Aquest codi compta fins a 100.
+        
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Número:"+i+".");
+        }
+    }   
+}
